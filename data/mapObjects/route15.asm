c17db11d102676778353b86c209e1f99584ca2ee
iopietro <iopietro@hotmail.it>
2018-11-06 00:25:09 +0100

    Bug fixes +Fixed a bug that locked the player in a infinite loop of battles with "XXXTentacion", at the end of the Route15

--- data/mapObjects/route15.asm
+++ data/mapObjects/route15.asm
@@ -8,20 +8,10 @@ Route15Object:
 	db $9, $e, $3, ROUTE_15_GATE_1F
 
 	db $1 ; signs
-	db $9, $27, $c ; Route15Text12
+	db $9, $27, $2 ; Route15Text2
 
 	db $1 ; objects
-	;object SPRITE_LASS, $29, $b, STAY, DOWN, $1, OPP_JR_TRAINER_F, $14
-	;object SPRITE_LASS, $35, $a, STAY, LEFT, $2, OPP_JR_TRAINER_F, $15
-	object SPRITE_BLACK_HAIR_BOY_1, $1f, $d, STAY, UP, $3, OPP_BIRD_KEEPER, $6
-	;object SPRITE_BLACK_HAIR_BOY_1, $23, $d, STAY, UP, $4, OPP_BIRD_KEEPER, $7
-	;object SPRITE_FOULARD_WOMAN, $35, $b, STAY, DOWN, $5, OPP_BEAUTY, $9
-	;object SPRITE_FOULARD_WOMAN, $29, $a, STAY, RIGHT, $6, OPP_BEAUTY, $a
-	;object SPRITE_BIKER, $30, $a, STAY, DOWN, $7, OPP_BIKER, $3
-	;object SPRITE_BIKER, $2e, $a, STAY, DOWN, $8, OPP_BIKER, $4
-	;object SPRITE_LASS, $25, $5, STAY, RIGHT, $9, OPP_JR_TRAINER_F, $16
-	;object SPRITE_LASS, $12, $d, STAY, UP, $a, OPP_JR_TRAINER_F, $17
-	;object SPRITE_BALL, $12, $5, STAY, NONE, $b, TM_20
+	object SPRITE_BLACK_HAIR_BOY_1, $1f, $d, STAY, UP, $1, OPP_BIRD_KEEPER, $6
 
 	; warp-to
 	EVENT_DISP ROUTE_15_WIDTH, $8, $7 ; ROUTE_15_GATE_1F
--- text/maps/route_15.asm
+++ text/maps/route_15.asm
@@ -1,171 +1,21 @@
 _Route15BattleText1::
-	text "Let me try out the"
-	line "#MON I just"
-	cont "got in a trade!"
+	text "Suicide if you"
+	line "ever try to"
+	cont "let go!"
 	done
 
 _Route15EndBattleText1::
-	text "Not"
-	line "good enough!"
+	text "Who"
+	line "i am?"
 	prompt
 
 _Route15AfterBattleText1::
-	text "You can't change"
-	line "the nickname of"
-	cont "any #MON you"
-	cont "get in a trade."
-
-	para "Only the Original"
-	line "Trainer can."
-	done
-
-_Route15BattleText2::
-	text "You look gentle,"
-	line "so I think I can"
-	cont "beat you!"
-	done
-
-_Route15EndBattleText2::
-	text "No,"
-	line "wrong!"
-	prompt
-
-_Route15AfterBattleText2::
-	text "I'm afraid of"
-	line "BIKERs, they look"
-	cont "so ugly and mean!"
-	done
-
-_Route15BattleText3::
-	text "Suicide if you"
-	line "ever try to"
-	cont "let go!"
-	done
-
-_Route15EndBattleText3::
-	text "Who"
-	line "i am?"
-	prompt
-
-_Route15AfterBattleText3::
 	text "I'm sad,"
 	line "i know,"
 	cont "yeah!"
 	done
 
-_Route15BattleText4::
-	text "Hmm? My birds are"
-	line "shivering! You're"
-	cont "good, aren't you?"
-	done
-
-_Route15EndBattleText4::
-	text "Just"
-	line "as I thought!"
-	prompt
-
-_Route15AfterBattleText4::
-	text "Did you know moves"
-	line "like EARTHQUAKE"
-	cont "don't have any"
-	cont "effect on birds?"
-	done
-
-_Route15BattleText5::
-	text "Oh, you're a"
-	line "little cutie!"
-	done
-
-_Route15EndBattleText5::
-	text "You looked"
-	line "so cute too!"
-	prompt
-
-_Route15AfterBattleText5::
-	text "I forgive you!"
-	line "I can take it!"
-	done
-
-_Route15BattleText6::
-	text "Suicide if you"
-	line "ever try to"
-	cont "let go!"
-	done
-
-_Route15EndBattleText6::
-	text "Who"
-	line "i am?"
-	prompt
-
-_Route15AfterBattleText6::
-	text "I'm sad,"
-	line "i know,"
-	cont "yeah!"
-	done
-
-_Route15BattleText7::
-	text "Hey kid! C'mon!"
-	line "I just got these!"
-	done
-
-_Route15EndBattleText7::
-	text "Why"
-	line "not?"
-	prompt
-
-_Route15AfterBattleText7::
-	text "You only live"
-	line "once, so I live"
-	cont "as an outlaw!"
-	cont "TEAM ROCKET RULES!"
-	done
-
-_Route15BattleText8::
-	text "Fork over all your"
-	line "cash when you"
-	cont "lose to me, kid!"
-	done
-
-_Route15EndBattleText8::
-	text "That"
-	line "can't be true!"
-	prompt
-
-_Route15AfterBattleText8::
-	text "I was just joking"
-	line "about the money!"
-	done
-
-_Route15BattleText9::
-	text "What's cool?"
-	line "Trading #MON!"
-	done
-
-_Route15EndBattleText9::
-	text "I"
-	line "said trade!"
-	prompt
-
-_Route15AfterBattleText9::
-	text "I trade #MON"
-	line "with my friends!"
-	done
-
-_Route15BattleText10::
-	text "Want to play with"
-	line "my #MON?"
-	done
-
-_Route15EndBattleText10::
-	text "I was"
-	line "too impatient!"
-	prompt
-
-_Route15AfterBattleText10::
-	text "I'll go train with"
-	line "weaker people.@@"
-
-_Route15Text12::
+_Route15Text2::
 	text "ROUTE 15"
 	line "West to FUCHSIA"
 	cont "CITY"
