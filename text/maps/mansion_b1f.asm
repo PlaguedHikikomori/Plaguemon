146a67266b1e2eabdd5631a593e3c52f3c72f8c0
PlaguedHikikomori <34514202+PlaguedHikikomori@users.noreply.github.com>
2018-11-07 16:28:05 +0100

    Cyber quest improvments
    + Storyline
    + Some script bug fix

--- text/maps/mansion_b1f.asm
+++ text/maps/mansion_b1f.asm
@@ -8,52 +8,57 @@ _Mansion4EndBattleText1::
 	prompt
 
 _Mansion4AfterBattleText1::
-	text "You can find stuff"
-	line "lying around."
+	text "I feel.."
+	line "controlled.."
 	done
 
 _Mansion4BattleText2::
-	text "This place is"
-	line "ideal for a lab."
+	text "I see..."
+	line "I SEE SOMETHING!"
 	done
 
 _Mansion4EndBattleText2::
 	text "What"
-	line "was that for?"
+	line "is this reality?"
 	prompt
 
 _Mansion4AfterBattleText2::
-	text "I like it here!"
-	line "It's conducive to"
-	cont "my studies!"
+	text "I understood"
+	line "what pain means."
 	done
 
 _Mansion4BattleText3::
-	text "Uh-oh. Where am"
-	line "I now?"
+	text "UGH UGH.."
 	done
 
 _Mansion4EndBattleText3::
-	text "Awooh!"
+	text "THE BRAIN.."
 	prompt
 
 _Mansion4AfterBattleText3::
-	text "You can find stuff"
-	line "lying around."
+	text "We're..."
+	line "friends..."
 	done
 
 _Mansion4BattleText4::
-	text "Uh-oh. Where am"
-	line "I now?"
+	text "After the disaster.."
+	line "Scientists"
+	cont "that were"
+	cont "experimenting on"
+	cont "us.."
+	cont "lefted the project.."
+	
+	para "We created THE BRAIN"
+	line "to rule us.."
 	done
 
 _Mansion4EndBattleText4::
-	text "Awooh!"
+	text "Now.."
 	prompt
 
 _Mansion4AfterBattleText4::
-	text "You can find stuff"
-	line "lying around."
+	text "We are its"
+	line "SLAVES!"
 	done
 
 _Mansion4Text7::
