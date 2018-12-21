217ccf9a53e54d88a0c0217a3ec3b740b821afaa
PlaguedHikikomori <34514202+PlaguedHikikomori@users.noreply.github.com>
2018-12-21 01:47:08 +0100

    Tutto rotto.
    + Some experiments on shmup
     (to turn down, comment -> call shmup)
    + some artificial sadness experiments
     (to turn off, comment -> callba InternalThought
    + Sine macro experiments

--- scripts/spaceworld.asm
+++ scripts/spaceworld.asm
@@ -1,12 +1,12 @@
 SpaceWorldScript:
-    ld a, REDS_HOUSE_2F
+	ld a, REDS_HOUSE_2F
 	ld [wLastMap], a
     call EnableAutoTextBoxDrawing
-	ld a, [hJoyPressed]
-	cp B_BUTTON
-	ret nz
-	;call Bullet2
+	call ProduceSine
+	pop de
+	call NPCBullet
 	ret
+	
 	
 Shmup1:
 
@@ -15,22 +15,97 @@ Shmup1End:
 	
 SpaceWorldTextPointers:
     dw PickUpItemText
-	
-	
-RightEnemyDistance:
-	ld a, [wOAMBuffer + 1]
-	ld c, a
-	ld a, [wOAMBuffer + 17]
-	sub c
-	ld d, a
+
+ProduceSine:	
+.Increase
+	ld a, [wSineCounter]
+	inc a
+	ld [wSineCounter] , a	
+.goDown
+	ld a, [wSineCounter] 
+	cp $10
+	jr z, .goUp
+	ld hl, wSpriteStateData1 + $14
+	ld de, wSpriteStateData1 + $14
+	push de
+	call GoDown
+	ld hl, wSpriteStateData1 + $24
+	pop de
+	call GoDown
 	ret
+.goUp
+	call GoUp
+	ld c, $1
+	call DelayFrames
+	ld a, [wSineCounter]
+	dec a
+	ld [wSineCounter] , a
+	jr nz, .goUp
+	jr .Increase
 	
-	
+GoDown:
+	;ld hl, wSpriteStateData1 + $14
+	call Compare16bit
+	push hl
+	ld a, [hl]                      
+	inc a 
+	jr nc, .notPlayerTwo
+	inc a
+.notPlayerTwo
+	ld [hl] , a                     
+	inc hl
+	inc hl
+	ld a, [hl]
+	dec a
+	ld [hl] , a
+	ret
+GoUp:	
+	ld hl, wSpriteStateData1 + $14
+	ld a, [hl] 
+	dec a
+	ld [hl] , a
+	inc hl 
+	inc hl
+	ld a, [hl] 
+	dec a
+	ld [hl] , a
+	ret
 
-    
-	
-	
+;Subtraction 16 bit routine  X - Y
+;INPUT:  X = DE  Y = HL  --> DE-HL
+;OUTPUT: Set carry flag if mismatch occurs.
+Compare16bit:
+	ld a, e
+	sub l
+	jr nz, .notEqual
+	ld a, d
+	sub h
+	ret z
+.notEqual
+	scf
+	ret
 
-    
-
+NPCBullet:
+	ld c, 9
+	push bc
+.bulletLoop
+	pop bc
+	ld a, $FF
+	ld [wUpdateSpritesEnabled], a
+	ld a, 9
+	pop de
+	call WriteOAMBlock
+	push bc
+	ld c, 30
+	call DelayFrames
+	ld a, $01
+	ld [wUpdateSpritesEnabled], a
+	call DelayFrame
+	call UpdateSprites
+	pop bc
+	ld a, c
+	dec a
+	ld c, a
+	jp nz, .bulletLoop
+	ret
 
